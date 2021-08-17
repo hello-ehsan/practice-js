3fc42ddc37c0cfdbcd3948d0c22a02a99613cc5d
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-08-17 17:57:51 +0600

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+function student(name = 'test', age = 0) {
+  console.log('My name is ' + name + '. I am ' + age);
+}
+student('Ehsan', 25);
+student('Who?', 0);
+student();
+
+console.log('a', { a: 'a' });
+console.log('a' + { a: 'a' });
